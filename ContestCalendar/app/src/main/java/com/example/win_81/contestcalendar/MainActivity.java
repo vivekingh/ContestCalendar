aa9315f22da49561a203e7724f0efee50a8ed924
Vivek Singh <singh108vivek@gmail.com>
2017-10-25 03:01:11 +0530

    spinner modified

--- ContestCalendar/app/src/main/java/com/example/win_81/contestcalendar/MainActivity.java
+++ ContestCalendar/app/src/main/java/com/example/win_81/contestcalendar/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.win_81.contestcalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("What's up ");
+        callSpinner();
+    }
+    void callSpinner(){
+        Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.contest_sites,R.layout.spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
